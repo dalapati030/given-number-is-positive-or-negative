69f034a3780228f6e164404cf90988174fe0ffde
dalapati030 <95222923+dalapati030@users.noreply.github.com>
2023-03-29 21:23:53 +0530

    Add files via upload

---
+++ Assignment.java
@@ -0,0 +1,17 @@
+import java.util.*;
+public class Assignment{
+public static void main(String args[])
+{
+Scanner sc=new Scanner(System.in);
+System.out.println("Enter the number");
+int n=sc.nextInt();
+if(n>0)
+{
+System.out.println("The number is positive");
+}
+else
+{
+System.out.println("The number is negative");
+}
+}
+}
